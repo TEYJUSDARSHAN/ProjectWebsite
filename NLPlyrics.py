07b79f3435d43218ecd1ba012550508af66679b8
TEYJUSDARSHAN <60425800+TEYJUSDARSHAN@users.noreply.github.com>
2020-12-24 01:19:59 +0530

    made working script for lyrics prediction

--- NLPlyrics.py
+++ NLPlyrics.py
@@ -1,23 +1,35 @@
+import tensorflow as tf
 from tensorflow import keras
-import pickle
 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+import numpy as np 
+
+import pickle
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-model = keras.models.load_model('path/to/location')
+model = keras.models.load_model('sheeran_npl_model.h5')
 
-def writelyrics(numwords,initial_sent):
-    seed_text = initial_sent
-    next_words = numwords
+
+
+seed_text = "i hear a song"
+next_words = 150
   
-    for _ in range(next_words):
-	    token_list = tokenizer.texts_to_sequences([seed_text])[0]
-	    token_list = pad_sequences([token_list], maxlen = 15, padding='pre')
-	    predicted = model.predict_classes(token_list, verbose=0)
-	    output_word = ""
-	    for word, index in tokenizer.word_index.items():
-		    if index == predicted:
-			    output_word = word
-			    break
-	    seed_text += " " + output_word
-    return seed_text
+for _ in range(next_words):
+	token_list = tokenizer.texts_to_sequences([seed_text])[0]
+	token_list = pad_sequences([token_list], maxlen=16-1, padding='pre')
+	predicted = model.predict_classes(token_list, verbose=0)
+	output_word = ""
+	for word, index in tokenizer.word_index.items():
+		if index == predicted:
+			output_word = word
+			break
+	seed_text += " " + output_word
+print(seed_text)
+
+
+
